0efeab689a7bf2e7dc07ac9b6fb9ce1cdef393fc
bansalankit11 <128984987+bansalankit11@users.noreply.github.com>
2023-04-30 10:12:12 +0530

    Add files via upload

---
+++ manage-profile.js
@@ -0,0 +1,28 @@
+const users = ['Ankit', 'Rohan', 'Aditya', 'Jack'];
+
+const memberDiv = document.querySelector('.memberDiv');
+const addIcon = document.querySelector('.addIcon');
+
+const userIcons = () => {
+    users.reverse();
+    users.map((curElem) => {
+        memberDiv.insertAdjacentHTML('afterbegin', `
+        <button class="btn"><span>${curElem}</span></button> 
+        `);
+    })
+};
+
+addIcon.addEventListener('click', () => {
+    let userName = prompt('please enter your name');
+
+    if(userName != null && !users.includes(userName)){
+        users.push(userName);
+        console.log(users);
+        memberDiv.insertAdjacentHTML('afterbegin', `
+        <button class="btn"><span>${userName}</span></button> 
+        `);
+    }else{
+        alert('user name already exist');
+    }
+})
+userIcons();
